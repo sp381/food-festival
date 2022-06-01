7c5267e1555417d4f7735579cafdf8e6a3815baf
sp381 <sarah.powell381@gmail.com>
2022-05-31 17:30:56 -0700

    updated JS files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+const img1 = require("../../assets/img/food-table.jpg");
+const img2 = require("../../assets/img/grill.jpg");
+
 $(document).ready(function() {
   
     const el = document.createElement(htmlString);
@@ -32,14 +35,14 @@ $(document).ready(function() {
     {
       title: "We travel all over the US",
       subtitle: "Check out our schedule!",
-      img: "./assets/img/food-table.jpg",
+      img: img1,
       btnText: "View Schedule",
       btnUrl: "schedule.html"
     },
     {
       title: "Our food is seriously the bomb!",
       subtitle: "What are you waiting for?",
-      img: "./assets/img/grill.jpg",
+      img: img2,
       btnText: "Purchase Tickets",
       btnUrl: "tickets.html"
     },
--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,43 @@ require("bootstrap");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './assets/js/script.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
-    },
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+      },
+
+      output: {
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist",
+      },
+      
+      module: {
+          rules: [
+              {
+                  test: /\.jpg$/i,
+                  use: [
+                      {
+                          loader: 'file-loader',
+                          options: {
+                              esModule: false,
+                              name (file) {
+                                  return "[path][name].[ext]"
+                              },
+                              publicPath: function(url) {
+                                  return url.replace("../", "/assets/")
+                              }
+                          }
+                      },
+                      {
+                          loader: 'image-webpack-loader'
+                      }
+                  ]
+              }
+          ]
+      },
+
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
@@ -18,6 +50,6 @@ module.exports = {
             analyzerMode: "static", // the report outputs to an HTML file in the dist folder
         })
     ],
-    
+
     mode: 'development'
 };
